fd5a36b7457897dfbb12568919b7fc4d3c0ba7f4
ohqay <158271932+ohqay@users.noreply.github.com>
2025-08-08 11:22:37 -0700

    Initial Commit

---
+++ codecontextTests/codecontextTests.swift
@@ -0,0 +1,17 @@
+//
+//  codecontextTests.swift
+//  codecontextTests
+//
+//  Created by Tarek Alexander on 08-08-2025.
+//
+
+import Testing
+@testable import codecontext
+
+struct codecontextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
